7c1c28bbcd25274ecc8e27e478a85c6916a307fa
Jaganmohan <jaganmohan@MacBook-Pro.local>
2020-08-05 12:41:10 +0530

    data download script added

---
+++ first.py
@@ -0,0 +1,33 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Variables that contains the user credentials to access Twitter API
+access_token = "your_access_token"
+access_token_secret = "your_access_token_secret"
+consumer_key = "your_consumer_key"
+consumer_secret = "your_consumer_secret"
+
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print (data)
+        return True
+
+    def on_error(self, status):
+        print (status)
+
+
+if __name__ == '__main__':
+
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+
+    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    stream.filter(track=['python', 'javascript', 'ruby'])
